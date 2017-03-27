c414613f8d43a09066698d225a1b4bae5f6d8dd9
Pixelastic <tim@pixelastic.com>
2017-03-27 23:08:54 +0200

    Update minify script to better handle selector limit

---
+++ scripts/minify.js
@@ -0,0 +1,71 @@
+import CleanCSS from 'clean-css';
+import _ from 'lodash';
+import Promise from 'bluebird';
+import fs from 'fs';
+
+const Minify = {
+  writeFile(path, content) {
+    return Promise.promisify(fs.writeFile)(path, content);
+  },
+
+  restructureRules(input) {
+    console.info('Restructure rules');
+    const options = {
+      level: {
+        1: true,
+        2: {
+          all: false,
+          removeEmpty: true,
+          restructureRules: true,
+        },
+      },
+    };
+    const instance = new CleanCSS(options);
+    // clean-css 4.0.10 does not allow updating it from the options directly
+    instance.options.compatibility.selectors.mergeLimit = 100;
+    return instance.minify(input).styles;
+  },
+
+  mergeNonAdjacent(input) {
+    console.info('Merge non-adjacent rules');
+    const options = {
+      level: {
+        2: {
+          all: false,
+          mergeNonAdjacentRules: true,
+        },
+      },
+    };
+    const instance = new CleanCSS(options);
+    return instance.minify(input).styles;
+  },
+
+  format(input) {
+    console.info('Format output');
+    const options = {
+      format: {
+        breaks: {
+          betweenSelectors: true,
+          afterRuleEnds: true,
+        },
+      },
+    };
+    const instance = new CleanCSS(options);
+    return instance.minify(input).styles;
+  },
+
+  run(args) {
+    args = _.slice(args, 2);
+    const inputFile = args[0] || './public/css/full.css';
+    const outputFile = args[1] || './public/css/full.min.css';
+
+    const input = fs.readFileSync(inputFile, 'utf-8');
+
+    let minified = Minify.restructureRules(input);
+    // minified = Minify.mergeNonAdjacent(minified);
+    minified = Minify.format(minified);
+    console.info('Write output to file');
+    Minify.writeFile(outputFile, minified);
+  },
+};
+Minify.run(process.argv);

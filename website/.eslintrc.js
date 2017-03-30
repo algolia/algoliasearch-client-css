50864bb4e5a5eb84056edb30bcf6c7e32a13b78b
Pixelastic <tim@pixelastic.com>
2017-03-31 00:29:18 +0200

    Adding download modal

---
+++ website/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  globals: {
+    "$": true,
+    "Favico": true,
+    "communityHeader": true,
+    "App": true
+  },
+  rules: {
+    'prefer-arrow-callback': 0,
+    'object-shorthand': 0,
+    'no-var': 0,
+    'no-console': 0,
+    'no-process-exit': 0,
+  },
+};
+
